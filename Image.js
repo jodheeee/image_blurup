df662fb976de27ae11fccd74ce741a94d225ae38
DaeHee <75158767+jodheeee@users.noreply.github.com>
2021-10-22 17:05:07 +0900

    Create Image.js

---
+++ Image.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import "./image.scss";
+function Image(props) {
+  const [loadingChk, setLoadingChk] = useState(false);
+  return (
+    <>
+      <img
+        className="image temp"
+        alt={props.alt}
+        src={props.temp}
+        style={{ visibility: isLoaded ? "hidden" : "visible" }}
+      />
+      <img
+        onLoad={() => {
+          setIsLoaded(true);
+        }}
+        className="image org"
+        style={{ opacity: isLoaded ? 1 : 0 }}
+        alt={props.alt}
+        src={props.src}
+      />
+    </>
+  );
+};
+export default Image;
